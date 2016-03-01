5735f9cc5535def6a0f2746ad537881682004476
Tim Jones <t.jones@amrc.co.uk>
2016-03-01 16:36:53 +0000

    new post_detail view etc.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, 'blog/post_list.html', {"posts":posts})
+
+def post_detail(request, pk):
+    # post = Post.objects.get(pk=post_id) - This works, but would return a gnarly error if post_id doesn't exist!
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
+    
