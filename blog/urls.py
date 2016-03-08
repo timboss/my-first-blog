f3567d908d7ad1a79f605be0262417d5c1045cd4
Tim Jones <t.jones@amrc.co.uk>
2016-03-08 17:49:46 +0000

    Improved security with auth decorators, logout etc. New icons

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
                url(r'^post/new/$', views.post_new, name="post_new"),
                url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
                url(r'^drafts/$', views.post_drafts, name="post_drafts"),
+               url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name="post_publish"),
+               url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name="post_remove"),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Login redirect
+LOGIN_REDIRECT_URL ="/"
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
