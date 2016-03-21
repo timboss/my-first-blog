082861f94de2ef5ec1c95a09bdece05bea079faa
Tim Jones <t.jones@amrc.co.uk>
2016-03-21 18:28:01 +0000

    Comment moderation working! DG Tutorial complete - Woooooooo!

--- blog/models.py
+++ blog/models.py
@@ -6,15 +6,16 @@ class Post(models.Model):
     author = models.ForeignKey("auth.User")
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField( default=timezone.now )
+    published_date = models.DateTimeField( blank=True, null=True )
     #publisher = models.ForeignKey("auth.User", blank=True, null=True) 
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+        
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.title
@@ -30,7 +31,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-        
+    
     def __str__(self):
         return self.text
-    
